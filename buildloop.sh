e0b3a4a38660fd87a4a6c3f682933d2d17537f46
folinu <samid36360@gmail.com>
2026-06-13 16:28:02 +0300

    rename wd

--- buildloop.sh
+++ buildloop.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-cd ~/raylib
+cd ~/pathgame
+echo 'printing current directory...'
+pwd
 while [ true ]; do
-	echo ---------------------------------------------------------------------------------------------------
-	rm a.out
-	gcc -g -lraylib main.c -Werror -Wfatal-errors -Wpedantic --pedantic-errors -Wall -Wextra -Wformat -pedantic -Wimplicit
-	./a.out
-	read
-	echo ---------------------------------------------------------------------------------------------------
-done;
+  echo ---------------------------------------------------------------------------------------------------
+  rm a.out
+  gcc -g -lraylib main.c -Werror -Wfatal-errors -Wpedantic --pedantic-errors -Wall -Wextra -Wformat -pedantic -Wimplicit
+  ./a.out
+  read
+  echo ---------------------------------------------------------------------------------------------------
+done
