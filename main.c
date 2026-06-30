b453a4a1f0faafc0ab83c95d959a8d9d7df14338
folinu <samid36360@gmail.com>
2026-06-30 19:47:36 +0300

    make all planets have russian annotation

--- main.c
+++ main.c
@@ -236,6 +236,10 @@ int main(void) {
 	printf("GetMonitorWidth(0)-windowBorder = %i\n", width);
 	printf("GetMonitorHeight(0)-windowBorder = %i\n", height);
 	SetWindowSize(width, height);
+	int codepoints[512] = { 0 };
+	for (int i = 0; i < 95; i++) codepoints[i] = 32 + i;
+	for (int i = 0; i < 255; i++) codepoints[96 + i] = 0x400 + i;
+	Font rusFont = LoadFontEx("font.ttf",12,codepoints,512);
 	// DRAWLOOP
 	while (APPSTATE != CLOSING) {
 		BeginDrawing();
@@ -247,6 +251,18 @@ int main(void) {
 			ClearBackground((Color){16,16,16,255});
 
 			DrawCircle(width/2, height/2, 24, YELLOW);		// SUN
+			if (clickCounter > 9) {
+				DrawTextEx(
+					rusFont,
+					"ТЫ",
+					(Vector2){
+					width/2+20,
+					height/2+20},
+					16,
+					1.0,
+					WHITE
+				);
+			};
 
 			DrawCircle(						// MERCURY
 				(width/2)+(48*sin(GetTime()*1.1 + 3)),		// x
@@ -254,6 +270,18 @@ int main(void) {
 				4,
 				WHITE
 			);
+			if (clickCounter > 8) {
+				DrawTextEx(
+					rusFont,
+					"МЕРКУРИЙ",
+					(Vector2){
+					(width/2)+(48*sin(GetTime()*1.1 + 3))+4,
+					(height/2)+(48*cos(GetTime()*1.1 + 3))+4},
+					16,
+					1.0,
+					WHITE
+				);
+			};
 
 			DrawCircle(						// VENUS
 				(width/2)+(64*sin(GetTime()*1.0 + 12)),		// x
@@ -261,6 +289,18 @@ int main(void) {
 				6,
 				(Color){255,100,15,255}
 			);
+			if (clickCounter > 7) {
+				DrawTextEx(
+					rusFont,
+					"ВЕНЕРА",
+					(Vector2){
+					(width/2)+(64*sin(GetTime()*1.0 + 12))+5,
+					(height/2)+(64*cos(GetTime()*1.0 + 12))+5},
+					16,
+					1.0,
+					WHITE
+				);
+			};
 
 			DrawCircle(						// EARTH
 				(width/2)+(80*sin(GetTime()*0.9 + 1)),		// x
@@ -268,6 +308,18 @@ int main(void) {
 				7,
 				(Color){15,70,255,255}
 			);
+			if (clickCounter > 6) {
+				DrawTextEx(
+					rusFont,
+					"ЗЕМЛЯ",
+					(Vector2){
+					(width/2)+(80*sin(GetTime()*0.9 + 1))+7,
+					(height/2)+(80*cos(GetTime()*0.9 + 1))+7},
+					16,
+					1.0,
+					WHITE
+				);
+			};
 
 			DrawCircle(						// MARS
 				(width/2)+(96*sin(GetTime()*0.8 + 42)),		// x
@@ -275,6 +327,18 @@ int main(void) {
 				7,
 				(Color){250,50,10,255}
 			);
+			if (clickCounter > 5) {
+				DrawTextEx(
+					rusFont,
+					"МАРС",
+					(Vector2){
+					(width/2)+(96*sin(GetTime()*0.8 + 42))+7,
+					(height/2)+(96*cos(GetTime()*0.8 + 42))+7},
+					16,
+					1.0,
+					WHITE
+				);
+			};
 
 			DrawCircle(						// JUPITER
 				(width/2)+(128*sin(GetTime()*0.7 + 23)),	// x
@@ -282,6 +346,18 @@ int main(void) {
 				13,
 				(Color){250,200,35,255}
 			);
+			if (clickCounter > 4) {
+				DrawTextEx(
+					rusFont,
+					"ЮПИТЕР",
+					(Vector2){
+					(width/2)+(128*sin(GetTime()*0.7 + 23))+11,
+					(height/2)+(128*cos(GetTime()*0.7 + 23))+11},
+					16,
+					1.0,
+					WHITE
+				);
+			};
 
 			DrawCircle(						// SATURN
 				(width/2)+(160*sin(GetTime()*0.6 + 7)),		// x
@@ -289,6 +365,18 @@ int main(void) {
 				13,
 				(Color){250,200,135,255}
 			);
+			if (clickCounter > 3) {
+				DrawTextEx(
+					rusFont,
+					"САТУРН",
+					(Vector2){
+					(width/2)+(160*sin(GetTime()*0.6 + 7))+11,
+					(height/2)+(160*cos(GetTime()*0.6 + 7))+11},
+					16,
+					1.0,
+					WHITE
+				);
+			};
 
 			DrawCircle(						// URANUS
 				(width/2)+(192*sin(GetTime()*0.5 + 86)),	// x
@@ -296,6 +384,18 @@ int main(void) {
 				6,
 				(Color){90,190,255,255}
 			);
+			if (clickCounter > 2) {
+				DrawTextEx(
+					rusFont,
+					"УРАН",
+					(Vector2){
+					(width/2)+(192*sin(GetTime()*0.5 + 86))+9,
+					(height/2)+(192*cos(GetTime()*0.5 + 86))+9},
+					16,
+					1.0,
+					WHITE
+				);
+			};
 
 			DrawCircle(						// NEPTUNE
 				(width/2)+(208*sin(GetTime()*0.4 + 13)),	// x
@@ -303,6 +403,18 @@ int main(void) {
 				7,
 				(Color){0,110,255,255}
 			);
+			if (clickCounter > 1) {
+				DrawTextEx(
+					rusFont,
+					"НЕПТУН",
+					(Vector2){
+					(width/2)+(208*sin(GetTime()*0.4 + 13))+9,
+					(height/2)+(208*cos(GetTime()*0.4 + 13))+9},
+					16,
+					1.0,
+					WHITE
+				);
+			};
 		};
 		processButtons();
 		if (APPSTATE == MAIN_MENU) {
