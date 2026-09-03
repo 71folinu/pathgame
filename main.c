ae75336bb3951fc42b2426116339e60d2eb1b312
folinu <samid36360@gmail.com>
2026-09-03 18:40:25 +0300

    try to look down

--- main.c
+++ main.c
@@ -5,29 +5,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-
 // Global variables
 bool playerFastRun = false;
 float ViewSensivity = 0.5;
-
 // Global constants
 const int BannerFontSize = 32;
 const int appFPS = 40;
 const int windowBorder = 32;
-
 // Global enums
 enum BUTTON_ANCHOR { UL, UR, LL, LR };
 enum APPSTATE_ENUM { CLOSING, LOADING_APP, MAIN_MENU, PLANET, PHYSICS_TEST, TRANSITION, PATHGAME, SETTINGS_SCREEN };
 // enum APPSTATE_ENUM APPSTATE = LOADING_APP;
-enum APPSTATE_ENUM APPSTATE = SETTINGS_SCREEN;
-
+// enum APPSTATE_ENUM APPSTATE = SETTINGS_SCREEN;
+enum APPSTATE_ENUM APPSTATE = PATHGAME;
 // Global appstate counters
 int loadingAppCounter = 0;
 int clickCounter = 0;
 int transitionCounter = 0;
-
 #include "buttons.c"
-
 // Draw banner in the middle of the screen. Used when loading for the first time
 void DrawBanner(char text[], int width, int height) {
 	int posx = width / 2 - MeasureText(text, BannerFontSize) / 2;
@@ -39,7 +34,6 @@ void DrawBanner(char text[], int width, int height) {
 	DARKGRAY);
 	DrawText(text, posx, posy, BannerFontSize, WHITE);
 }
-
 // Draw streetlight
 void DrawStreetlight(Vector3 BasePos) {
 	Color StreetlightColor = (Color){40,40,50,255};
@@ -104,12 +98,10 @@ void DrawStreetlight(Vector3 BasePos) {
 		LitPathColor
 	);
 }
-
 // Entrypoint
 int main(void) {
 	printf("ENTERED MAIN\n");
 	InitWindow(1, 1, "App");
-
 	// Window setup
 	SetTargetFPS(appFPS);
 	width = GetMonitorWidth(0) - windowBorder;
@@ -117,19 +109,18 @@ int main(void) {
 	printf("GetMonitorWidth(0)-windowBorder = %i\n", width);
 	printf("GetMonitorHeight(0)-windowBorder = %i\n", height);
 	SetWindowSize(width, height);
-
 	// Font setup for PLANET
 	int codepoints[512] = { 0 };
 	for (int i = 0; i < 95; i++) codepoints[i] = 32 + i;		// The standard codepoints for english
 	for (int i = 0; i < 255; i++) codepoints[96 + i] = 0x400 + i;	// Codepoints for russian
 	Font rusFont = LoadFontEx("font.ttf",12,codepoints,512);
-
 	// Setup for pathgame
 	bool EnableDrawFPS = true;
 	float playerSpeed = 0;
 	float playerWalkSpeed = 0.1;
 	float playerRunSpeed = 0.2;
 	float playerRotation = 90;
+	float playerTilt = 0;
 	float playerRotationSpeed = 8;
 	float playerHeight = 1.4;
 	float renderDist = 512;
@@ -140,22 +131,18 @@ int main(void) {
 	playerCamera.fovy = 70.0f;					// Camera field-of-view Y
 	playerCamera.projection = CAMERA_PERSPECTIVE;			// Camera mode type
 	Model PoleModel = LoadModel("pole.glb");
-
 	#include "physicsTestVars.c"
 	while (APPSTATE != CLOSING) {
 		BeginDrawing();
-
 		// Settings screen
 		if (APPSTATE == SETTINGS_SCREEN) {
 			ClearBackground(DARKGRAY);
 			processSettingsScreenButtons();
 			if (IsKeyPressed(KEY_ESCAPE)) APPSTATE = TRANSITION;
 		};
-
 		// Pathgame
 		if (APPSTATE == PATHGAME) {
 			if (IsKeyPressed(KEY_ESCAPE)) APPSTATE = TRANSITION;
-
 			if (IsKeyDown(KEY_LEFT_SHIFT)) {
 				if (playerFastRun == true) {
 					playerSpeed = playerRunSpeed*5;
@@ -165,7 +152,6 @@ int main(void) {
 			} else {
 				playerSpeed = playerWalkSpeed;
 			};
-
 			// Player movement processing
 			if (IsKeyDown(KEY_W)) {
 				playerCamera.position.x += sin(playerRotation*DEG2RAD)*playerSpeed;
@@ -183,14 +169,18 @@ int main(void) {
 				playerCamera.position.z -= sin(playerRotation*DEG2RAD)*playerSpeed;
 				playerCamera.position.x += cos(playerRotation*DEG2RAD)*playerSpeed;
 			};
-
-			// Player rotation processing
+			// Simple player rotation processing
 			if (IsKeyDown(KEY_H) || IsKeyDown(KEY_LEFT)) playerRotation += playerRotationSpeed*ViewSensivity;
 			if (IsKeyDown(KEY_L) || IsKeyDown(KEY_RIGHT)) playerRotation -= playerRotationSpeed*ViewSensivity;
+			// Mouse look processing
+			playerRotation -= (GetMouseX()-width/2)/4;
+			playerTilt -= (GetMouseY()-height/2)/4;
+								printf("%f",playerTilt);
+			SetMousePosition(width/2,height/2);
 			playerCamera.target = (Vector3) {	playerCamera.position.x+sin(playerRotation*DEG2RAD),
 								playerHeight,
 								playerCamera.position.z+cos(playerRotation*DEG2RAD)};
-
+			// Rendering
 			ClearBackground((Color){0,0,31,255});
 			BeginMode3D(playerCamera);
 				// Simple player shadow
@@ -202,10 +192,8 @@ int main(void) {
 				DrawCube((Vector3){0,0,0},renderDist,0.01,2.0,(Color){ 40, 40, 40, 255 });
 				// Dirt path
 				DrawCube((Vector3){-renderDist/4,0,0},renderDist/2,0.02,3,(Color){ 40, 20, 20, 255 });
-
 				// Modelled pole
 				DrawModel(PoleModel,(Vector3){8,0,0},1.0,WHITE);
-
 				// Streetlights
 				for (int i = 0; i < 32-6; i++) {
 					if (i == 6) {continue;};
@@ -228,17 +216,14 @@ int main(void) {
 				DrawFPS(10,10);
 			};
 		};
-
 		#include "physicsTest.c"
 		#include "planet.c"
-
 		// Main menu
 		if (APPSTATE == MAIN_MENU) {
 			ClearBackground(DARKGRAY);
 			processMainMenuButtons();
 			if (IsKeyPressed(KEY_ESCAPE)) APPSTATE = CLOSING;
 		};
-
 		// Loading screen after launch
 		if (APPSTATE == LOADING_APP) {
 			ClearBackground(BLACK);
@@ -250,7 +235,6 @@ int main(void) {
 			loadingAppCounter += 1;
 			printf("loadingAppCounter = %i\n", loadingAppCounter);
 		};
-
 		// Transition between a substate and main menu
 		if (APPSTATE == TRANSITION) {
 			transitionCounter += 1;
@@ -259,7 +243,6 @@ int main(void) {
 				transitionCounter = 0;
 			};
 		};
-
 		EndDrawing();
 	};
 	CloseWindow();
